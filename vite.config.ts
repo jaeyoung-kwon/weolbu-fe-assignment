f8c21149fce699983bb56c1996b71aef00bea6f4
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2026-01-10 21:31:45 +0900

    fix: emotion Component Selector 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
+    tanstackRouter(),
     react({
-      jsxImportSource: '@emotion/react',
+      plugins: [['@swc/plugin-emotion', {}]],
     }),
   ],
   server: {
